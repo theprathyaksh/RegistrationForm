e19a40c160ce8c0436eddcf3fe090fcdf092b8f9
theprathyaksh <iamprathyaksh@gmail.com>
2023-04-03 07:43:50 +0530

    first commit

---
+++ src/com/mysql/jdbc/PreparedStatement.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mysql.jdbc;
+
+/**
+ *
+ * @author theprathyaksh
+ */
+public class PreparedStatement {
+
+    public void setString(int i, String mobile) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
